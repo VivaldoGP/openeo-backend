d9f14458481797f0e3eb05405dd1d284b579faae
VivaldoGP <VivaldoGP27@hotmail.com>
2024-10-31 22:01:23 -0600

    feat: user register endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routers import openeo_routes, openeo_auth
+from app.routers import openeo_routes, openeo_auth, sign_up
 
 app = FastAPI()
 app.include_router(openeo_routes.router)
 app.include_router(openeo_auth.router)
+app.include_router(sign_up.router)
 
 @app.get("/")
 async def root():
